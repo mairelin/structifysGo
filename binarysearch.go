8fc2af290c66cc5a603d4ed0f33ab24a625c6197
mairelin <mairelinroque@gmail.com>
2020-02-28 16:42:28 -0400

    binary search

---
+++ binarysearch.go
@@ -0,0 +1,25 @@
+package main
+
+func BinarySearch(nums []int, target int) int {
+	var min int
+	var max = len(nums) - 1
+	var guess = 0
+
+	for min <= max {
+		guess = (min + max) / 2
+
+		switch {
+		case nums[guess] == target:
+			return guess
+		case nums[guess] < target:
+			min = guess + 1
+		default:
+			max = guess - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	println(BinarySearch([]int{1, 3}, 3))
+}
