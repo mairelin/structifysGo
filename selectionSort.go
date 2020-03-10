a2e8092be8e8aa726cd9c9984fc6a67887600d04
mairelin <mairelinroque@gmail.com>
2020-03-10 01:30:54 -0400

    adding more comments

--- selectionSort.go
+++ selectionSort.go
@@ -3,16 +3,21 @@ package main
 func SelectionSort(input []int) []int {
 	var aux, minval, minIndex int
 
+	//iterates the array to be sort
 	for index, minvalue := range input {
 		minval = minvalue
 		minIndex = index
 
+		//look for the minimun element in the array
 		for i := minIndex + 1; i < len(input); i++ {
 			if input[i] < minval {
+				//select the new minimun element
 				minval = input[i]
 				minIndex = i
 			}
 		}
+
+		//swipe the values
 		aux = input[index]
 		input[index] = minval
 		input[minIndex] = aux
